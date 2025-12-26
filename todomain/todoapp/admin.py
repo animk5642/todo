47053871f7050ba00fd3b9c134f7661d262bf677
animk5642 <animk5642@gmail.com>
2025-12-26 12:00:20 +0530

    addtasktemplatae1

--- todomain/todoapp/admin.py
+++ todomain/todoapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from todoapp.models  import Task
+from todoapp.models import Task
 
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'is_complited', 'created_at','updated_at')
+    search_fields = ('task',)
+    
+admin.site.register(Task, TaskAdmin)
 
 # Register your models here.
